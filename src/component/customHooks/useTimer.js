f5227ba2ef4a0180e1a4d5907b6b26093a387ecc
Abhihsek <abhishekanand.jnv@gmail.com>
2024-09-21 13:48:45 +0530

    jkhhk

--- src/component/customHooks/useTimer.js
+++ src/component/customHooks/useTimer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function useTimer(time) {
   const [countDown, setCountDown] = useState(time);
