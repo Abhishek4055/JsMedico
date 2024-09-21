b5b1ab1f43caa177c80f66c11d8f886f42daf058
Abhihsek <abhishekanand.jnv@gmail.com>
2024-09-21 12:41:09 +0530

    package name changes

--- src/App.js
+++ src/App.js
@@ -46,6 +46,27 @@ export default function App() {
     },
   ]);
 
+  // function Test() {}
+
+  // Test.prototype.x = function () {
+  //   return "x";
+  // };
+
+  // function Test2() {
+  //   Test.call(this); // Call the Test constructor inside Test2
+  // }
+
+  // // Set Test2's prototype to inherit from Test
+  // Test2.prototype = Object.create(Test.prototype);
+
+  // // Fix the constructor reference for Test2
+  // Test2.prototype.constructor = Test2;
+
+  // let we = new Test2();
+  // console.log(we.y(), we.x()); // Outputs: "x"
+
+  // console.log(Test2.prototype);
+
   return (
     <>
       <RouterProvider router={routerPath} />
